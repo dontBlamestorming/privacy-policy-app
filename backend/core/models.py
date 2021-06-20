4edaa0e96ee623aed6bda683f91abd89260c735a
Ukdong <lklone20005@summercat.co>
2021-06-21 02:10:50 +0900

    add core_model null and blank config

--- backend/core/models.py
+++ backend/core/models.py
@@ -7,24 +7,35 @@ class PrivacyPolicyForm(models.Model):
         ("male", "Male"),
         ("female", "Female"),
     )
+
     receiver = models.ForeignKey(
-        "account.User", related_name="form", on_delete=models.CASCADE
+        "account.User",
+        related_name="form",
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
     )
     studio = models.ForeignKey(
         "account.Studio", db_column="studio_id", on_delete=models.CASCADE
     )
-    name = models.CharField(_("Name"), max_length=16)
-    email = models.EmailField(_("Email address"), max_length=128)
-    birthday = models.CharField(_("Birthday"), max_length=16, default="")
-    gender = models.CharField(_("Gender"), max_length=8, choices=GENDER_CHOICES)
-    phone = models.CharField(_("Phone number"), max_length=16)
+    name = models.CharField(_("Name"), max_length=16, null=False, blank=False)
+    email = models.EmailField(
+        _("Email address"), max_length=128, null=False, blank=False
+    )
+    birthday = models.CharField(_("Birthday"), max_length=16, null=False, blank=False)
+    gender = models.CharField(
+        _("Gender"), max_length=8, choices=GENDER_CHOICES, null=False, blank=False
+    )
+    phone = models.CharField(_("Phone number"), max_length=16, null=False, blank=False)
     created = models.DateTimeField(_("Created"), auto_now_add=True)
-    sign = models.FileField(_("Sign image"), upload_to="files/")
+    sign = models.FileField(
+        _("Sign image"), upload_to="files/", null=False, blank=False
+    )
 
 
 class Image(models.Model):
     user = models.ForeignKey(
-        "account.User", related_name="file", on_delete=models.CASCADE
+        "account.User", related_name="file", on_delete=models.CASCADE, null=False
     )
-    form = models.ForeignKey("PrivacyPolicyForm", on_delete=models.CASCADE)
-    file = models.FileField(_("PSD image"), upload_to="psd/")
+    form = models.ForeignKey("PrivacyPolicyForm", on_delete=models.CASCADE, null=False)
+    file = models.FileField(_("PSD image"), upload_to="psd/", null=False, blank=True)
